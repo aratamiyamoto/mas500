3b40dcf70293aa0782e218201aeaffb63c1bef7a
Arata Miyamoto <artimo14@Aratas-MacBook-Pro.local>
2014-11-20 00:37:43 -0500

    added router.js

---
+++ assignment02_web/app/router.js
@@ -0,0 +1,31 @@
+var splashRouter;
+
+(function($){
+
+var SplashRouter = Backbone.Router.extend({
+
+    routes: {
+      "": "home",
+      "country/:country": "country",
+    },
+    
+    initialize: function(){
+        _.bindAll(this)
+    },
+     
+    home: function() {
+        console.log('home()');
+    },
+
+    country: function(country) {
+        console.log('country()');
+        $('#gv-country').val(country);
+        splashView.lookupCountry2();
+    }
+});
+
+splashRouter = new SplashRouter;
+
+})(jQuery);
+
+Backbone.history.start({pushState: true});
